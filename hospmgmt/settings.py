7e5e8566cdfa44c9b3d41303c6cf5f175a85c178
devyanshm <devyasnhmishra0@gmail.com>
2025-08-20 17:11:33 +0530

    Your commit message

--- hospmgmt/settings.py
+++ hospmgmt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-xjgu2*r2t-b39g%kwh9*hf#cr9zfpedwe3bsxv-25pjg0$$u0w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
